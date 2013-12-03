eea5b34ae4e53271afdf05acddd43597b8171869
xuezu <ruoqi.drq@alibaba-inc.com>
2013-12-03 19:36:39 +0800

    update

---
+++ index.js
@@ -0,0 +1,5 @@
+var GITHUB_API = 'https:\/\/api\.github\.com'
+var exec = require('child_process').exec
+
+var api = [GITHUB_API, 'repos', 'luckydrq', ''].join('\/')
+exec(['curl ', GITHUB_API, ''])
